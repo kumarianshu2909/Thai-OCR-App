5e5317a33312199728236639174950882c5680e7
Kumari Anshu <kumarianshu2909@gmail.com>
2023-12-25 22:51:49 +0530

    Adding frontend files

---
+++ front_end/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageInputComponent from './ImageInputComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>OCR Detection App</h1>
+      <ImageInputComponent />
+    </div>
+  );
+};
+
+export default App;
